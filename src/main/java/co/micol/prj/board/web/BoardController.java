5b2c0ca6cd9f4493b4333218643df3795be354b9
'YJH' <'duwlgud05@naver.com'>
2022-01-12 17:53:08 +0900

    2022.01.12

--- src/main/java/co/micol/prj/board/web/BoardController.java
+++ src/main/java/co/micol/prj/board/web/BoardController.java
@@ -26,8 +26,6 @@ public class BoardController {
 	@Autowired
 	private BcommentService bcommentDao;
 	
-	@Autowired
-	private String saveDir; //파일저장 경로를 자동 주입
 	
 	@RequestMapping("boardList.do")
 	public String boardSelectList(Model model) {
@@ -42,28 +40,15 @@ public class BoardController {
 	
 	@RequestMapping("boardInsert.do")
 	public String boardInsert(@RequestParam("file") MultipartFile file, BoardVO board, Model model, HttpSession session) {
-		String originalFileName = file.getOriginalFilename();
-		//String saveDir = "fileUpload" + File.separatorChar; //실서버로 베포할때 저장공간, 그때 열면됨
-		if(!originalFileName.isEmpty()) {
-			String uuid = UUID.randomUUID().toString();
-			//uuid에 파일확장자 추가하여 물리적 파일명을 만듬
-			String saveFileName = uuid + originalFileName.substring(originalFileName.lastIndexOf("."));
-			try {
-				file.transferTo(new File(saveDir, saveFileName));
-				board.setBoard_picture(originalFileName);
-				board.setBoard_pfile(saveFileName);
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
+		
 		session.setAttribute("member_id", "test@abc.com");
 		//board = (BoardVO)session.getAttribute("");
 		board.setBoard_id((String) session.getAttribute("member_id"));
 		int r = boardDao.boardInsert(board);
 		if(r > 0) {
-			model.addAttribute("message", "등록성공");
+			model.addAttribute("message", "�벑濡앹꽦怨�");
 		} else {
-			model.addAttribute("message", "등록실패");
+			model.addAttribute("message", "�벑濡앹떎�뙣");
 		}
 		model.addAllAttributes(boardDao.boardSelectList());
 		return "ogani/board/boardList";
@@ -78,20 +63,7 @@ public class BoardController {
 	
 	@RequestMapping("boardUpdate.do")
 	public String boardUpdate(@RequestParam("file") MultipartFile file, Model model, BoardVO board) {
-		String originalFileName = file.getOriginalFilename();
-		//String saveDir = "fileUpload" + File.separatorChar; //실서버로 베포할때 저장공간, 그때 열면됨
-		if(!originalFileName.isEmpty()) {
-			String uuid = UUID.randomUUID().toString();
-			//uuid에 파일확장자 추가하여 물리적 파일명을 만듬
-			String saveFileName = uuid + originalFileName.substring(originalFileName.lastIndexOf("."));
-			try {
-				file.transferTo(new File(saveDir, saveFileName));
-				board.setBoard_picture(originalFileName);
-				board.setBoard_pfile(saveFileName);
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
+		
 		
 		model.addAttribute(boardDao.boardUpdate(board));
 		return "ogani/board/boardUpdateSuccess";
